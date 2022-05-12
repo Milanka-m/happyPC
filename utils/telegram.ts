5ab3111541ef16c6bb8f83e4f263158105d49692
Milana Birulia <lianabir@mail.ru>
2022-05-12 15:45:38 +0300

    create project happyPC

--- utils/telegram.ts
+++ utils/telegram.ts
@@ -1,8 +1,8 @@
 const sendTelegramm = async (text: string): Promise<void> => {
-	const chat_id: number = -1001711484353
+	const chat_id: number = -11111111111
 	try {
 		await fetch(
-			`https://api.telegram.org/bot5377356728:AAFzkxg2-Vx0rJE6kg_yIjnPDVJahZ4kJbQ/sendMessage?chat_id=${chat_id}&text=${text}`,
+			`https://api.telegram.org/bot1111111111:AAFzkxg2-Vx0rJE6kg_yIjnPDVJahZ4kJbQ/sendMessage?chat_id=${chat_id}&text=${text}`,
 		)
 	} catch (error) {
 		throw new Error(`Ошибка в отправке сообщения`)
